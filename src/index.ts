d0e0ead98166c6c6280980a9dfb36c444b3203bd
Dmitry2489 <iva.klicunov2489@mail.ru>
2022-10-03 20:54:40 +0300

    Added crud get videos

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,23 @@ import express, {Request, Response} from 'express'
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+const videosData = [
+    {
+        id: 0,
+        title: "string",
+        author: "string",
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        createdAt: "2022-10-03T17:50:20.179Z",
+        publicationDate: "2022-10-03T17:50:20.179Z",
+        availableResolutions: [
+            "P144"
+        ]
+    }
+]
+
+app.get('/videos', (req: Request, res: Response) => {
+    res.send(videosData)
 })
 
 app.listen(port, () => {
