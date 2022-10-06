a6ecbdc18ad92fcbfaaf3b4e7e6955b05876062c
Dmitry2489 <iva.klicunov2489@mail.ru>
2022-10-06 21:33:58 +0300

    Added crud get videos delete

--- src/index.ts
+++ src/index.ts
@@ -16,83 +16,78 @@ const videosData = [
             "P144"
         ]
     }
-]
+];
 
 app.get('/videos', (req: Request, res: Response) => {
-    res.send(videosData)
-})
+    res.send(videosData);
+});
 
-app.post('/videos', (req: Request, res: Response) => {
-    const titleVideo = req.body.title
-    const authorVideo = req.body.author
-    const availableResolutions = req.body.availableResolutions
+app.post('/videos', (req, res) => {
+    const titleVideo = req.body.title;
+    const authorVideo = req.body.author;
+    const availableResolutions = req.body.availableResolutions;
 
-    if (!req.body.title || req.body.title == null) {
-        res.status(400).json(
-            {
-                "errorsMessages": [
-                    {
-                        "message": "Title is required",
-                        "field": "title"
-                    }
-                ],
-            }
-        )
-        return
+    if (!req.body.title) {
+        res.status(400).json({
+            "errorsMessages": [
+                {
+                    "message": "Title is required",
+                    "field": "title"
+                }
+            ],
+        });
+        return;
     }
 
     const newVideo = {
         id: +(new Date()),
         title: titleVideo,
-        author: "string",
+        author: authorVideo,
         canBeDownloaded: true,
         minAgeRestriction: null,
         createdAt: String(new Date()),
-        publicationDate: String(new Date()),
-        availableResolutions: [
-            "P144"
-        ]
-    }
-    videosData.push(newVideo)
+        publicationDate: new Date().toISOString(),
+        availableResolutions: availableResolutions
+    };
+
+    videosData.push(newVideo);
     return newVideo;
-})
+});
 
-app.get('/videos/:videoId', (req: Request, res: Response) => {
-    const id = +req.params.videoId
-
-    const video = videosData.find(v => v.id === id)
+app.get('/videos/:videoId', (req, res) => {
+    const id = +req.params.videoId;
+    const video = videosData.find(v => v.id === id);
 
     if (!video) {
-        res.sendStatus(404)
+        res.sendStatus(404);
+        return;
     }
 
-    res.status(201).send(video)
-})
-
-app.delete('/videos/:videoId', (req: Request, res: Response) => {
-    const id = +req.params.videoId
-
-    const index = videosData.findIndex(v => v.id === id)
-
+    res.status(201).send(video);
+});
+app.delete('/videos/:videoId', (req, res) => {
+    const id = +req.params.videoId;
+    const index = videosData.findIndex(v => v.id === id);
 
     if (index === -1) {
-        res.sendStatus(404)
+        res.sendStatus(404);
+        return;
     } else {
-        videosData.splice(index, 1)
-        res.sendStatus(204)
+        videosData.splice(index, 1);
+        res.sendStatus(204);
+        return;
     }
-})
+});
 
-app.delete('/testing/all-data', (req: Request, res: Response) => {
-
-    const index = videosData.splice(0, -1)
-
+app.delete('/testing/all-data', (req, res) => {
+    const index = videosData.splice(0, -1);
 
     if (index.length === 0) {
-        res.sendStatus(204)
+        res.sendStatus(204);
+        return;
     }
-})
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
