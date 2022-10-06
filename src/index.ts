be1b5890d892a260bc681920337d9e5cde5c0e9b
Dmitry2489 <iva.klicunov2489@mail.ru>
2022-10-06 20:57:24 +0300

    delete testing all-data

--- src/index.ts
+++ src/index.ts
@@ -22,13 +22,48 @@ app.get('/videos', (req: Request, res: Response) => {
     res.send(videosData)
 })
 
+app.post('/videos', (req: Request, res: Response) => {
+    const titleVideo = req.body.title
+    const authorVideo = req.body.author
+    const availableResolutions = req.body.availableResolutions
+
+    if (!req.body.title || req.body.title == null) {
+        res.status(400).json(
+            {
+                "errorsMessages": [
+                    {
+                        "message": "Title is required",
+                        "field": "title"
+                    }
+                ],
+            }
+        )
+        return
+    }
+
+    const newVideo = {
+        id: +(new Date()),
+        title: titleVideo,
+        author: "string",
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        createdAt: String(new Date()),
+        publicationDate: String(new Date()),
+        availableResolutions: [
+            "P144"
+        ]
+    }
+    videosData.push(newVideo)
+    return newVideo;
+})
+
 app.get('/videos/:videoId', (req: Request, res: Response) => {
     const id = +req.params.videoId
 
     const video = videosData.find(v => v.id === id)
 
-    if(!video) {
-       res.sendStatus(404)
+    if (!video) {
+        res.sendStatus(404)
     }
 
     res.status(201).send(video)
@@ -40,10 +75,20 @@ app.delete('/videos/:videoId', (req: Request, res: Response) => {
     const index = videosData.findIndex(v => v.id === id)
 
 
-    if(index === -1) {
+    if (index === -1) {
         res.sendStatus(404)
     } else {
         videosData.splice(index, 1)
+        res.sendStatus(204)
+    }
+})
+
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+
+    const index = videosData.splice(0, -1)
+
+
+    if (index.length === 0) {
         res.sendStatus(204)
     }
 })
